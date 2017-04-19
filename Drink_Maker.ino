3db7cbdab9e7d709960a9739b17066d67a91df3a
Mikey Torres <mrmojorisen0@gmail.com>
2017-04-19 17:22:50 -0400

    New upload
    Potential code fixes for EEPROM. hopefully work

--- Drink_Maker.ino
+++ Drink_Maker.ino
@@ -26,22 +26,136 @@ const int bPinRight = 3;
 const int bPinTop = 4;
 const int bPinBot = 5;
 const int bPinLeft = 6;
+const int NAME = 0;
+const int LIQUID1 = 1;
+const int LIQUID2 = 3;
+const int LIQUID3 = 5;
+const int l1OZ = 2;
+const int l2OZ = 4;
+const int l3OZ = 6;
+//left drinks[drinkselection--][NAME]
+//right drink[drinkselection++][NAME]
 
 void setup() {
-  oled.begin();
-  oled.clear(ALL);
-  oled.display();
   pinMode(bPinTop, INPUT_PULLUP);
   pinMode(bPinLeft, INPUT_PULLUP);
   pinMode(bPinRight, INPUT_PULLUP);
   pinMode(bPinBot, INPUT_PULLUP);
+  oled.begin();
+  oled.clear(ALL);
+  oled.display();
+  Serial.begin(9600);
+  while(!Serial) {;}
+  delay(SEC);
+  //initializeEEPROMData();
+  readDrinkData();
 }
 
 void loop() {
-menu();
-menuSelect();
+  menu();
+  menuSelect();
 }
 
+//------------------------------------------------------------------
+/*
+ * Initializes the data in eeprom
+*/
+void initializeEEPROMData() {
+  String tdrinks[numDrinks][numInfo] = { 
+    {"drink1", "1", "2", "3", "2", "0", "0"},
+    {"drink2", "1", "4", "1", "1", "7", "3"},
+    {"drink3", "3", "4", "2", "1", "2", "2"},
+    {"drink3", "0", "0", "1", "3", "4", "1"},
+    {"drink4", "0", "0", "3", "2", "4", "2"},
+    {"drink5", "4", "2", "9", "2", "5", "3"}
+  };
+  char c;
+  int address;
+  int column = 0;
+  int beginC = 0;
+  int endC;
+  int drink = 0;
+  String info;
+
+  /* Add to EEPROM */
+  for (address = 0; drink < numDrinks-1; address++) {
+    /* Reached the end of a line */
+    if (column > numInfo-1) {
+      drink++;
+      column = NAME;
+      Serial.print("address=");
+      Serial.println(address);
+      Serial.print("New drink\ndrink=");
+      Serial.println(drink);
+      EEPROM.write(address, '\n');
+    }
+    info = tdrinks[drink][column];
+    Serial.println(info);
+    /* Add characters from info into EEPROM */
+    for (int index = 0; ; index++, address++) {
+      if (index > info.length()-1) {/* Reached the end of string */
+        Serial.print("address=");
+        Serial.println(address);
+        Serial.println("done writing info");
+        EEPROM.write(address, ' ');
+        column++;
+        break;
+      }
+      c = info.charAt(index);
+      Serial.print("address=");
+      Serial.println(address);
+      Serial.print("let=\"");
+      Serial.print(c);
+      Serial.println('\"');
+      EEPROM.write(address, c);
+    }
+  }
+}
+
+//------------------------------------------------------------------
+/*
+ * Read the drink data from eeprom
+*/
+void readDrinkData() {
+  char c;
+  int address;
+  int column = 0;
+  int drink = 0;
+  String info;  // The data in each column
+  
+  for(address = 0; drink < numDrinks-1; address++) {
+    c = EEPROM.read(address);
+    Serial.print("let=");
+    Serial.println(c);
+    Serial.print("address=");
+    Serial.println(address);
+    
+    /* Reached the end of a line */
+    if (c == '\n') {
+      drink++;
+      Serial.print("new drink\ndrink=");
+      Serial.println(drink);
+    }
+    /* Reached the end of a string */
+    if (c == ' ') {
+      Serial.println("reached end of string");
+      Serial.print("info=\"");
+      Serial.print(info);
+      Serial.println('\"');
+      drinks[drink][column] = info;
+      info.remove(0);
+      column++;
+      Serial.print("column=");
+      Serial.println(column);
+    } else /* Add to the current read letter to the info string */
+      info += c;
+  }
+}
+
+//------------------------------------------------------------------
+/*
+ * Print a menu on screen
+*/
 void menu() {
   oled.clear(PAGE);
   oled.setFontType(0);
@@ -50,8 +164,12 @@ void menu() {
   oled.display();
 }
 
+//------------------------------------------------------------------
+/*
+ * Controller for the menu.
+ */
 void menuSelect() {
-  for (;;){
+  for (;;){ /* wait for input */
     if (digitalRead(bPinLeft) == LOW) {
       oled.clear(PAGE);
       oled.print("Test1\n");
@@ -80,6 +198,18 @@ void menuSelect() {
   }
 }
 
-void joyStick() {
+//------------------------------------------------------------------
+/*
+ * Utility function for joystick.
+*/
+int xjoyStick() {
+}
+
+//------------------------------------------------------------------
+/*
+ * Print options for selecting a drink.
+*/
+void selDrink() {
+  
 }
 
